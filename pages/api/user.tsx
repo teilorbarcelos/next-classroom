5833292437f3bbbabdec8ebb7a60859b9270db74
Teilor Souza Barcelos <tsb-@live.com>
2021-07-17 12:00:26 -0300

    test API

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -3,21 +3,13 @@ import connect from "../../utils/database"
 
 export default async (req: NextApiRequest, resp: NextApiResponse) => {
 
-  const validateParam = (parameter: string) => {
-    let valid = false
-    if(parameter.trim() != ''){
-      valid = true
-    }
-    return valid
-  }
-
-  if(req.method === 'POST'){
+  if(req.method === 'GET'){
     const {name, email, cellPhone, teacher} = req.body
 
-    if(!validateParam(name) ||
-      !validateParam(email) ||
-      !validateParam(cellPhone) ||
-      !validateParam(teacher)){
+    if(!name ||
+      !email ||
+      !cellPhone ||
+      !teacher){
 
       resp.status(400).json({error: 'Missing some information!'})
       return
@@ -26,14 +18,14 @@ export default async (req: NextApiRequest, resp: NextApiResponse) => {
 
     const { db } = await connect()
 
-    const response = await db.collection('users').insertOne({
-      name,
-      email,
-      cellPhone,
-      teacher
-    })
+    // const response = await db.collection('users').insertOne({
+    //   name,
+    //   email,
+    //   cellPhone,
+    //   teacher
+    // })
     
-    // const response = await db.collection('users').findOne({email: email})
+    const response = await db.collection('users').findOne({email: email})
     
     resp.status(200).json(response)
 
