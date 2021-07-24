ef47179f2b9f2a2b04b10de7c5d309bee5f56d2c
Teilor Souza Barcelos <tsb-@live.com>
2021-07-24 17:32:23 -0300

    geting available hours by useState

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -12,9 +12,14 @@ const Profile: NextPage = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [cellphone, setCellphone] = useState('')
-  const [courses, setCourses] = useState([''])
-  const [availableLocations, setAvailableLocations] = useState([''])
-  const [availableHours, setAvailableHours] = useState<Record<string, number[]>>({})
+  const [courses, setCourses] = useState('')
+  const [availableLocations, setAvailableLocations] = useState('')
+  const [monday, setMonday] = useState('')
+  const [tuesday, setTuesday] = useState('')
+  const [wednesday, setWednesday] = useState('')
+  const [thursday, setThursday] = useState('')
+  const [friday, setFriday] = useState('')
+
   const [ session, loading ] = useSession()
   const { data, error } = useSWR(`/api/user/${session?.user.email}`, api)
 
@@ -60,22 +65,37 @@ const Profile: NextPage = () => {
             placeholder="E-mail"
             className="bg-pink-200 px-2 my-4"
           />
-          <input type="cellphone" placeholder="Cellphone" className="bg-pink-200 px-2 my-4" />
+          <input
+            type="cellphone"
+            value={cellphone}
+            onChange={(e) => setCellphone(e.target.value)}
+            placeholder="Cellphone"
+            className="bg-pink-200 px-2 my-4"
+          />
           <div className="my-4">
             <label htmlFor="teacher" className="mx-2">Desejo ser professor:</label>
-            <input type="checkbox" id="teacher" name="teacher" onClick={() => isTeacher ? setIsTeacher(false) : setIsTeacher(true)} />
+            <input
+              type="checkbox"
+              id="teacher"
+              name="teacher"
+              onClick={() => isTeacher ? setIsTeacher(false) : setIsTeacher(true)}
+            />
           </div>
           {isTeacher && (
             <>
               <h1>Matérias que você domina (separadas por vírgula)</h1>
               <input
                 type="text"
+                value={courses}
+                onChange={(e) => setCourses(e.target.value)}
                 placeholder="Engenharia Mecatrônica, Astrofísica..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
               <h1>Locais onde você pode dar aulas:</h1>
               <input
                 type="text"
+                value={availableLocations}
+                onChange={(e) => setAvailableLocations(e.target.value)}
                 placeholder="Ex.: https://letmeask-f38c5.web.app , em casa..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
@@ -83,30 +103,40 @@ const Profile: NextPage = () => {
               <h2>Segunda:</h2>
               <input
                 type="text"
+                value={monday}
+                onChange={(e) => setMonday(e.target.value)}
                 placeholder="Ex.: 8, 10, 14,16..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
               <h2>Terça:</h2>
               <input
                 type="text"
+                value={tuesday}
+                onChange={(e) => setTuesday(e.target.value)}
                 placeholder="Ex.: 8, 10, 14,16..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
               <h2>Quarta:</h2>
               <input
                 type="text"
+                value={wednesday}
+                onChange={(e) => setWednesday(e.target.value)}
                 placeholder="Ex.: 8, 10, 14,16..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
               <h2>Quinta:</h2>
               <input
                 type="text"
+                value={thursday}
+                onChange={(e) => setThursday(e.target.value)}
                 placeholder="Ex.: 8, 10, 14,16..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
               <h2>Sexta:</h2>
               <input
                 type="text"
+                value={friday}
+                onChange={(e) => setFriday(e.target.value)}
                 placeholder="Ex.: 8, 10, 14,16..."
                 className="bg-pink-200 w-full px-2 my-4"
               />
